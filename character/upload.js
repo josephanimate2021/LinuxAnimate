be783dbe9bedf1a8b69fb21d864935758ff50ea9
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-17 20:12:55 -0400

    Update upload.js

--- character/upload.js
+++ character/upload.js
@@ -19,9 +19,12 @@ module.exports = function (req, res, url) {
 		var numId = fUtil.getNextFileId("char-", ".xml");
 		parse.unpackXml(buffer, `c-${numId}`);
 		fs.unlinkSync(path);
-
+		const xml = Buffer.from(buffer);
+		const tIDbeg = xml.indexOf('" theme_id="') + 12;
+		const tIDend = xml.indexOf('" x="');
+		const themeId = xml.subarray(tIDbeg, tIDend).toString();
 		res.statusCode = 302;
-		var url = `/cc?original_asset_id=${numId}`;
+		var url = `/cc?original_asset_id=c-${numId}&themeId=${themeId}`;
 		res.setHeader("Location", url);
 		res.end();
 	});
