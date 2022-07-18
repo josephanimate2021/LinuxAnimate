584e93c82b412f1f0ff5da7c55e2d4dc6c416d9f
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-17 21:49:54 -0400

    Update load.js

--- movie/load.js
+++ movie/load.js
@@ -57,6 +57,7 @@ module.exports = function (req, res, url) {
 				case "/ajax/deleteChar/":
 				case "/ajax/deleteMovie/": {
 					movie.delete(url.query.movieId);
+					movie.deleteThumb(url.query.movieId);
 					return true;
 				}
 			}
