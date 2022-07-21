b0230f409ff5aef5c2a1549567bb9fdecdd874f1
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-21 15:37:09 -0400

    Update watermark.js

--- asset/watermark.js
+++ asset/watermark.js
@@ -10,48 +10,11 @@ module.exports = function (req, res, url) {
 	if (req.method != 'POST') return;
 
 	var makeZip = false; switch (url.path) {
-		case '/goapi/getUserAssets/': makeZip = true; break;
-		case '/goapi/getUserAssetsXml/': break;
+		case '/goapi/getUserWatermarks/': break;
 		default: return;
 	}
-
-	loadPost(req, res).then(async data => {
-		var xmlString;
-		switch (data.type) {
-			case 'char': {
-				const chars = await user.loadChars(data.themeId);
-				xmlString = `${header}<ugc more="0">${chars.map(v => `<char id="${v.id}" name="Untitled" cc_theme_id="${
-					v.theme}" thumbnail_url="char_default.png" copyable="Y"><tags/></char>`).join('')}</ugc>`;
-				break;
-			}
-			case 'bg': {
-				xmlString = `${header} <ugc more="0"><bg id="666.jpg" /></ugc>`;
-				break;
-			}
-			case 'prop':
-			default: {
-				xmlString = `${header} <ugc more="0"><prop id="666" /></ugc>`;
-				break;
-			}
-		};
-
-		if (makeZip) {
-			const zip = nodezip.create();
-			fUtil.addToZip(zip, 'desc.xml', Buffer.from(xmlString));
-
-			switch (data.type) {
-				case 'bg': {
-					fUtil.addToZip(zip, 'bg/666.jpg', fs.readFileSync(`file: http://2.bp.blogspot.com/-hegG5mMd9kE/T9Y4CWZ6udI/AAAAAAAAA2I/nm-9Wlrh6a4/s1600/full-hd-wallpapers-1080p-1.jpg`));
-					break;
-				}
-			};
-			res.setHeader('Content-Type', 'application/zip');
-			res.end(Buffer.concat([base, await zip.zip()]));
-		}
-		else {
-			res.setHeader('Content-Type', 'text/xml');
-			res.end(xmlString);
-		}
-	})
+	var xmlString = `<watermarks><watermark id="174tbqdo0cs6" thumbnail="${process.env.WATERMARKS_FOLDER}/Go4Schools.png"/><preview>174tbqdo0cs6</preview><watermark id="52ht3dd60csd" thumbnail="${process.env.WATERMARKS_FOLDER}/GoMakeYourOwn.png"/><preview>52ht3dd60csd</preview></watermarks>`;
+	res.setHeader('Content-Type', 'text/xml');
+	res.end(xmlString);
 	return true;
 }
