b7c27f01d4366048d2a8071e6e903f5ce502136e
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-21 11:42:40 -0400

    Update main.js

--- watermark/main.js
+++ watermark/main.js
@@ -1,5 +1,6 @@
 const cachéFolder = process.env.CACHÉ_FOLDER;
-const folder = process.env.SAVED_FOLDER;
+var path = require("path");
+var folder = path.join(__dirname, "../../", process.env.SAVED_FOLDER);
 const exFolder = process.env.EXAMPLE_FOLDER;
 const caché = require("../asset/caché");
 const fUtil = require("../misc/file");
@@ -10,23 +11,20 @@ const fs = require("fs");
 
 module.exports = {
 	save(mId, wId) {
-		var path = `${folder}/${mId}-watermark.xml`;
+		var pth = path.join(folder, `${mId}.xml`);
 		var wXml;
 		if (wId == "0dhteqDBt5nY") {
 			wXml = '<watermarks><watermark style="visualplugin"/></watermarks>';
 		} else {
 			wXml = '<watermarks></watermarks>';
 		}
-		fs.writeFileSync(path, wXml);
+		fs.writeFileSync(pth, wXml);
 	},
 	meta(movieId) {
 		return new Promise(async (res, rej) => {
-			if (!movieId.startsWith("w-")) return;
-			const n = Number.parseInt(movieId.substr(2));
-			const fn = fUtil.getFileIndex("watermark-", ".xml", n);
+			const buffer = fs.readFileSync(path.join(folder, `${movieId}.xml`));
 
-			const fd = fs.openSync(fn, "r");
-			const buffer = Buffer.alloc(256);
+			const fd = fs.openSync(buffer, "r");
 			fs.readSync(fd, buffer, 0, 256, 0);
 
 			fs.closeSync(fd);
