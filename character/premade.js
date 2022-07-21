2125a5374cd041cfbd57ec1367be98a84803a498
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-21 17:08:33 -0400

    Update premade.js

--- character/premade.js
+++ character/premade.js
@@ -1,22 +1,5 @@
-const loadPost = require("../misc/post_body");
-const folder = process.env.WATERMARKS_FOLDER;
-const fs = require("fs");
-const http = require("http");
-
 module.exports = function (req, res, url) {
-	if (req.method != "POST") return;
-	switch (url.pathanme) {
-		case "/goapi/getCCPreMadeCharacters": {
-			res.end();
-			return true;
-		}
-		case "/goapi/getMovieInfo/": {
-			loadPost(req, res).then(([data]) => {
-				res.setHeader("Content-Type", "text/html; charset=UTF-8");
-				const p = `${folder}/${data.movieId}.xml`;
-				fs.createReadStream(p).pipe(res);
-			});
-			return true;
-		}
-	}
+	if (req.method != "POST" || url.path != "/goapi/getCCPreMadeCharacters") return; 
+	res.end();
+	return true;
 };
