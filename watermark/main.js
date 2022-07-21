dd6ae63b3fb143cb8e426c5037352617117e9f48
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-21 14:35:59 -0400

    Update main.js

--- watermark/main.js
+++ watermark/main.js
@@ -1,11 +1,4 @@
-const cachéFolder = process.env.CACHÉ_FOLDER;
 const folder = process.env.WATERMARKS_FOLDER;
-const exFolder = process.env.EXAMPLE_FOLDER;
-const caché = require("../asset/caché");
-const fUtil = require("../misc/file");
-const nodezip = require("node-zip");
-const parse = require("../movie/parse");
-const util = require("../misc/util");
 const fs = require("fs");
 
 module.exports = {
@@ -27,20 +20,5 @@ module.exports = {
 			}
 		}
 		fs.writeFileSync(path, wXml);
-	},
-	meta(movieId) {
-		return new Promise(async (res, rej) => {
-			const fn = `${folder}/${movieId}.xml`;
-
-			const fd = fs.openSync(fn, "r");
-			const buffer = Buffer.alloc(256);
-			fs.readSync(fd, buffer, 0, 256, 0);
-
-			fs.closeSync(fd);
-			res({
-				date: fs.statSync(fn).mtime,
-				id: movieId,
-			});
-		});
 	},
 };
