80e522e5db6516a3c8346ca792d05489354bce28
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-21 13:21:35 -0400

    Update main.js

--- watermark/main.js
+++ watermark/main.js
@@ -10,7 +10,7 @@ const fs = require("fs");
 
 module.exports = {
 	save(mId, wId) {
-		var path = `${folder}/${mId}.xml`;
+		var path = `${folder}/${mId}.txt`;
 		var wXml;
 		if (wId == "0dhteqDBt5nY") {
 			wXml = '<watermarks><watermark style="visualplugin"/></watermarks>';
