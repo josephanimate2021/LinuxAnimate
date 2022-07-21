f7629749a3c75b922df217aecc2f6b51d66ee86a
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-20 20:26:19 -0400

    Update watermark.js

--- movie/watermark.js
+++ movie/watermark.js
@@ -1,5 +1,4 @@
-const header = process.env.XML_HEADER;
-const movie = require("./main");
+const watermark = require("../asset/main");
 
 /**
  * @param {http.IncomingMessage} req
@@ -8,14 +7,11 @@ const movie = require("./main");
  * @returns {boolean}
  */
 module.exports = function (req, res, url) {
-	switch (req.method) {
-		case "POST": {
-			const match = req.url.match(/\/goapi\/assignwatermark\/movie\/([^.]+)\/([^.]+)$/);
-			if (!match) return;
-			var id = match[1];
-			var wId = match[2];
-			movie.watermark(id, wId);
-			break;
-		}
-	}
+	if (req.method != "POST") return;
+	const match = req.url.match(/\/goapi\/assignwatermark\/movie\/([^.]+)\/([^.]+)$/);
+	if (!match) return;
+	var id = match[1];
+	var wId = match[2];
+	var mode;
+	watermark.save(id, wId, mode = "watermark");
 }
