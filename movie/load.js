bf2c9eb5e5e8a08277b757acc3f6baf807e653b9
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-21 16:48:03 -0400

    Update load.js

--- movie/load.js
+++ movie/load.js
@@ -60,21 +60,6 @@ module.exports = function (req, res, url) {
 					movie.loadZip(url.query.movieId).then((b) => res.end(Buffer.concat([base, b]))).catch(() => res.end("1"));
 					return true;
 				}
-				case "/goapi/getMovieInfo/": {
-					// by default, this will read the goanimate free trial watermark. if a user chooses a watermark in the lvm, then it will then read the user's watermark.
-					var readStream = fs.createReadStream(path.join(__dirname, '../', process.env.WATERMARKS_FOLDER) + '/watermarks.txt', 'utf8');
-					let data = ''
-					readStream.on('data', function(chunk) {
-						data += chunk;
-					}).on('error', function(err) {
-						console.log('Error:', err);
-					}).on('end', function() {
-						// read the xml on console log to tell the user what watermark they are using.
-						console.log(`<?xml encoding="UTF-8"?>${data}`);
-						res.end(`<?xml encoding="UTF-8"?>${data}`);
-					});
-					return true;
-				}
 				case "/ajax/deleteStarter/":
 				case "/ajax/deleteChar/":
 				case "/ajax/deleteMovie/": {
