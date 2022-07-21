27fa5d00c64acc5903b7e96944fd38020be289c9
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-21 16:23:37 -0400

    Update watermark.js

--- asset/watermark.js
+++ asset/watermark.js
@@ -5,7 +5,7 @@ module.exports = function (req, res, url) {
 		case '/goapi/getUserWatermarks/': break;
 		default: return;
 	}
-	var xmlString = `<?xml encoding="UTF-8"?><watermarks><watermark id="174tbqdo0cs6" thumbnail="${process.env.WATERMARKS_FOLDER}/Go4Schools.png" name="GoAnimate For Schools Logo"/><preview>174tbqdo0cs6</preview><watermark id="52ht3dd60csd" thumbnail="${process.env.WATERMARKS_FOLDER}/GoMakeYourOwn.png" name="GoAnimate - Go Make Your Own Logo"/><preview>52ht3dd60csd</preview></watermarks>`;
+	var xmlString = `<?xml encoding="UTF-8"?><watermarks><watermark id="174tbqdo0cs6" thumbnail="${process.env.WATERMARKS_FOLDER}/Go4Schools.png" name="GoAnimate For Schools Logo"/><preview>174tbqdo0cs6</preview><watermark id="82tkgqdefbw6" thumbnail="${process.env.WATERMARKS_FOLDER}/freeTrial.png" name="GoAnimate Free Trial Logo"/><preview>82tkgqdefbw6</preview><watermark id="52ht3dd60csd" thumbnail="${process.env.WATERMARKS_FOLDER}/GoMakeYourOwn.png" name="GoAnimate - Go Make Your Own Logo"/><preview>52ht3dd60csd</preview></watermarks>`;
 	res.setHeader('Content-Type', 'text/xml');
 	res.end(xmlString);
 	return true;
