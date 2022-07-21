acf808d797ee14cdc57ac854ad4f8dbb58be94fb
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-21 15:54:42 -0400

    Update watermark.js

--- asset/watermark.js
+++ asset/watermark.js
@@ -1,3 +1,10 @@
+const header = process.env.XML_HEADER;
+const fUtil = require('../fileUtil');
+const nodezip = require('node-zip');
+const base = Buffer.alloc(1, 0);
+const user = require('./main');
+const fs = require('fs');
+
 module.exports = function (req, res, url) {
 	if (req.method != 'POST') return;
 
@@ -5,7 +12,7 @@ module.exports = function (req, res, url) {
 		case '/goapi/getUserWatermarks/': break;
 		default: return;
 	}
-	var xmlString = `<?xml encoding="UTF-8"?><watermarks><watermark id="174tbqdo0cs6" name="GoAnimate For Schools Logo" thumbnail="${process.env.WATERMARKS_FOLDER}/Go4Schools.png"/><preview>174tbqdo0cs6</preview><watermark id="52ht3dd60csd" thumbnail="${process.env.WATERMARKS_FOLDER}/GoMakeYourOwn.png" name="Go Make Your Own Logo/><preview>52ht3dd60csd</preview></watermarks>`;
+	var xmlString = `<?xml encoding="UTF-8"?><watermarks><watermark id="174tbqdo0cs6" thumbnail="${process.env.WATERMARKS_FOLDER}/Go4Schools.png"/><preview>174tbqdo0cs6</preview><watermark id="52ht3dd60csd" thumbnail="${process.env.WATERMARKS_FOLDER}/GoMakeYourOwn.png"/><preview>52ht3dd60csd</preview></watermarks>`;
 	res.setHeader('Content-Type', 'text/xml');
 	res.end(xmlString);
 	return true;
