a97e7e09eaf0158e153a1567f69373844fc8fdd8
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-21 02:50:25 -0400

    Update load.js

--- watermark/load.js
+++ watermark/load.js
@@ -1,8 +1,4 @@
-const fUtil = require("../misc/file");
-const base = Buffer.alloc(1, 0);
 const fs = require("fs");
-const movie = require("../movie/main");
-const watermark = require("./main");
 
 /**
  * @param {http.IncomingMessage} req
@@ -12,52 +8,8 @@ const watermark = require("./main");
  */
 module.exports = function (req, res, url) {
 	var path = url.pathname;
-	switch (req.method) {
-		case "GET": {
-			const match = req.url.match(/\/movies\/([^.]+)(?:\.(zip|xml))?$/);
-			if (!match) return;
-
-			var id = match[1];
-			var ext = match[2];
-			switch (ext) {
-				case "zip":
-					res.setHeader("Content-Type", "application/zip");
-					movie.loadZip(id).then((v) => {
-						if (v) {
-							res.statusCode = 200;
-							res.end(v);
-						} else {
-							res.statusCode = 404;
-							res.end();
-						}
-					});
-					break;
-				default:
-					res.setHeader("Content-Type", "text/xml");
-					movie.loadXml(id).then((v) => {
-						if (v) {
-							res.statusCode = 200;
-							res.end(v);
-						} else {
-							res.statusCode = 404;
-							res.end();
-						}
-					});
-					break;
-			}
-			return true;
-		}
-
-		case "POST": {
-			switch (path) {
-				case "/goapi/getMovieInfo/": {
-					res.setHeader("Content-Type", "text/xml");
-					const wId = watermark.load(url.query.movieId);
-					res.end(wId);
-					break;
-				}
-			}
-			return true;
-		}
-	}
+	if (req.method != "POST" || path != "/goapi/getMovieInfo/") return;
+	res.setHeader("Content-Type", "text/xml");
+	const wId = fs.readFileSync(`${process.env.SAVED_FOLDER}/${url.query.movieId}-watermark.xml`);
+	res.end(wId);
 };
