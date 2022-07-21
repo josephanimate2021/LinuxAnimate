e0f40835dc17e5f494b5683b815957d4a74bd9e8
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-20 23:12:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -17,8 +17,6 @@ const asL = require("./asset/list");
 const ast = require("./asset/thmb");
 const mvl = require("./movie/load");
 const mvL = require("./movie/list");
-const Wtr = require("./movie/watermark");
-const wtr = require("./asset/watermark");
 const MvL = require("./starter/list");
 const stu = require("./starter/upload");
 const mvm = require("./movie/meta");
@@ -31,7 +29,7 @@ const tsv = require("./tts/voices");
 const tsl = require("./tts/load");
 const url = require("url");
 
-const functions = [mvL, chu, Wtr, wtr, stu, Chl, pmc, asl, Mvt, MvL, chl, thl, thL, chs, cht, asL, tsl, chr, ast, mvm, mvl, mvs, mvt, tsv, asu, mvu, stp, stl];
+const functions = [mvL, chu, Wtr, stu, Chl, pmc, asl, Mvt, MvL, chl, thl, thL, chs, cht, asL, tsl, chr, ast, mvm, mvl, mvs, mvt, tsv, asu, mvu, stp, stl];
 
 module.exports = http
 	.createServer((req, res) => {
