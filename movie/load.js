2464c350cb8d07bad600f1167dfaa07d1a361fbb
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-21 15:07:36 -0400

    Update load.js

--- movie/load.js
+++ movie/load.js
@@ -54,12 +54,8 @@ module.exports = function (req, res, url) {
 					res.setHeader("Content-Type", "application/zip");
 
 					movie.loadZip(url.query.movieId).then(b => {
-						res.end(Buffer.concat([base, b])).catch((err, e) => {
-							if (!e) {
-								console.log(err);
-							} else {
-								console.log(e);
-							}
+						res.end(Buffer.concat([base, b])).catch(e => {
+							console.log(e);
 							res.end("1");
 						});
 					});
