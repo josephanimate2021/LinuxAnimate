0cdae0665034405b1060e12fc025bef307d757ce
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-21 03:22:30 -0400

    Update load.js

--- watermark/load.js
+++ watermark/load.js
@@ -2,8 +2,8 @@ var fs = require("fs");
 var path = require("path");
 var folder = path.join(__dirname, "../../", process.env.SAVED_FOLDER);
 module.exports = function (req, res, url) {
-	if (req.method != "POST" || pth != "/goapi/getMovieInfo/") return;
 	var pth = url.pathname;
+	if (req.method != "POST" || pth != "/goapi/getMovieInfo/") return;
 	var mId = url.query.movieId;
 	var filepath = path.join(folder, `${mId}-watermark.xml`);
 	var xml = fs.readFileSync(filepath);
