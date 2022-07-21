f122274bb5ca54f22a391be5f40a9f165814826b
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-20 22:28:50 -0400

    Update watermark.js

--- asset/watermark.js
+++ asset/watermark.js
@@ -1,25 +1,27 @@
 const loadPost = require('../request/post_body');
 const header = process.env.XML_HEADER;
-const fUtil = require('../fileUtil');
-const nodezip = require('node-zip');
-const base = Buffer.alloc(1, 0);
-const user = require('./main');
-const fs = require('fs');
 
 module.exports = function (req, res, url) {
 	if (req.method != 'POST') return;
 
-	var makeZip = false; switch (url.path) {
+	switch (url.path) {
 		case '/goapi/getUserWatermarks/': break;
 		default: return;
 	}
 
 	loadPost(req, res).then(async data => {
-		var xmlString, files;
+		var xmlString, files, content;
 		files = asset.list(data.movieId, "watermark");
-		xmlString = `${header}<watermarks>${files.map((v) => `<watermark id="${v.id}" thumbnail="${process.env.WATERMARKS_FOLDER}/${v.id}"/>`).join("")}</watermarks>`;
-		res.setHeader('Content-Type', 'text/xml');
-		res.end(xmlString);
+		files.map(v => {
+			if (v.id == "0dhteqDBt5nY") {
+				content = `<watermark id="0dhteqDBt5nY"/>`;
+			} else {
+				content = `<watermark id="${v.id} thumbnail="${process.env.WATERMARKS_FOLDER}/${v.id}"/>`;
+			}
+			xmlString = `${header}<watermarks>${content}</watermarks>`;
+			res.setHeader('Content-Type', 'text/xml');
+			res.end(xmlString);
+		});
 	};
 	return true;
 }
