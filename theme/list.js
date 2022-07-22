25a12e2ced27d66043926c9846bc5015feb823db
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-22 18:00:55 -0400

    Update list.js

--- theme/list.js
+++ theme/list.js
@@ -1,45 +1,5 @@
-/* Looking for a written form of the themes list?
-"action"
-"akon"
-"animal"
-"anime"
-"ben10"
-"bizmodels"
-"botdf"
-"bunny"
-"business"
-"cc_store"
-"chibi"
-"chowder"
-"christmas"
-"common"
-"commoncraft"
-"custom"
-"domo"
-"fullenergy"
-"infographics"
-"monkeytalk"
-"monstermsh"
-"ninja"
-"ninjaanime"
-"politic"
-"politics2"
-"retro"
-"sf"
-"space"
-"spacecitizen"
-"startrek"
-"stick"
-"sticklybiz"
-"street"
-"underdog"
-"vietnam"
-"whiteboard"
-"willie"
-*/
-
 const http = require("http");
-const fUtil = require("../misc/file");
+const fs = require("fs");
 const folder = process.env.THEME_FOLDER;
 
 /**
@@ -50,7 +10,7 @@ const folder = process.env.THEME_FOLDER;
  */
 module.exports = function (req, res, url) {
 	if (req.method != "POST" || url.path != "/goapi/getThemeList/") return;
-	res.setHeader("Content-Type", "application/zip");
-	fUtil.makeZip(`${folder}/_themelist.xml`, "themelist.xml").then((b) => res.end(b));
+	res.setHeader("Content-Type", "text/xml");
+	fs.readFileSync(`${folder}/_themelist.xml`);
 	return true;
 };
