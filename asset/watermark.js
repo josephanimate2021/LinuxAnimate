fd4797be656f4f69a250231435de95446b2bda97
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-21 22:03:05 -0400

    Update watermark.js

--- asset/watermark.js
+++ asset/watermark.js
@@ -13,7 +13,7 @@ module.exports = function (req, res, url) {
 		var xmlString = `<?xml encoding="UTF-8"?><watermarks><watermark id="174tbqdo0cs6" thumbnail="${process.env.WATERMARKS_FOLDER}/Go4Schools.png" name="GoAnimate For Schools Logo"/><preview>174tbqdo0cs6</preview><watermark id="82tkgqdefbw6" thumbnail="${process.env.WATERMARKS_FOLDER}/freeTrial.png" name="GoAnimate Free Trial Logo"/><preview>82tkgqdefbw6</preview><watermark id="52ht3dd60csd" thumbnail="${process.env.WATERMARKS_FOLDER}/GoMakeYourOwn.png" name="GoAnimate - Go Make Your Own Logo"/><preview>52ht3dd60csd</preview>${files.map(v => `<watermark id="${v.id}" thumbnail="${process.env.WATERMARKS_FOLDER}/${v.id}" name="Custom Logo"/><preview>${v.id}</preview>`).join("")}</watermarks>`;
 		res.setHeader('Content-Type', 'text/xml');
 		console.log(xmlString);
-		res.end(Buffer.from(xmlString));
+		res.end(xmlString);
 		return true;
 	});
 }
