0494bbc752fde6ff5f2e4ddfa7160649c3063a76
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-22 18:25:12 -0400

    Update list.js

--- theme/list.js
+++ theme/list.js
@@ -2,6 +2,14 @@ const http = require("http");
 const fUtil = require("../misc/file");
 const folder = process.env.THEME_FOLDER + "/";
 
+async function makeList(req, res, url) {
+	res.setHeader("Content-Type", "application/zip");
+	const path = folder + "themelist.xml";
+	fUtil.makeZip(path, "themelist.xml").then((b) => res.end(b));
+	return true;
+}
+	
+
 /**
  * @param {http.IncomingMessage} req
  * @param {http.ServerResponse} res
@@ -10,8 +18,5 @@ const folder = process.env.THEME_FOLDER + "/";
  */
 module.exports = function (req, res, url) {
 	if (req.method != "POST" || url.path != "/goapi/getThemeList/") return;
-	res.setHeader("Content-Type", "application/zip");
-	const path = folder + "themelist.xml";
-	fUtil.makeZip(path, "themelist.xml").then((b) => res.end(b));
-	return true;
+	await makeList(req, res, url);
 };
