e7be7a7e0f752b79bffb5754d0e5a5105ff11802
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-22 17:54:53 -0400

    Update load.js

--- theme/load.js
+++ theme/load.js
@@ -7,17 +7,17 @@ const http = require("http");
 module.exports = function (req, res, url) {
 	if (req.method != "POST" || url.path != "/goapi/getTheme/") return; 
 	loadPost(req, res).then(([data]) => {
-		const initcb = data.initcb;
-		const type = initcb ? 'application/zip' : 'text/xml';
+		data.initcb = false;
+		const type = data.initcb ? 'text/xml' : 'application/zip';
 		res.setHeader('Content-Type', type);
 		const p = `${folder}/${data.themeId}.xml`;
-		if (!initcb) {
-			// load lvm
-			fUtil.makeZip(p, 'theme.xml').then(b => res.end(b));
-			initcb = "studioLoaded";
-		} else {
+		if (data.initcb) {
 			// search themes
 			fs.createReadStream(p).pipe(res);
+		} else {
+			// load lvm
+			fUtil.makeZip(p, 'theme.xml').then(b => res.end(b));
+			data.initcb = true;
 		}
 	});
 	return true;
