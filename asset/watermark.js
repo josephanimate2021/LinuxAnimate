72fef1eedee6ad9eb77865a9db4a5a56e144cf97
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-22 13:31:14 -0400

    Update watermark.js

--- asset/watermark.js
+++ asset/watermark.js
@@ -9,8 +9,7 @@ async function listAssets(data) {
 	<?xml encoding="UTF-8"?><watermarks><watermark id="174tbqdo0cs6" thumbnail="${
 	process.env.WATERMARKS_FOLDER}/Go4Schools.png"/><preview>174tbqdo0cs6</preview><watermark id="82tkgqdefbw6" thumbnail="${
 	process.env.WATERMARKS_FOLDER}/freeTrial.png"/><preview>82tkgqdefbw6</preview><watermark id="52ht3dd60csd" thumbnail="${
-	process.env.WATERMARKS_FOLDER}/GoMakeYourOwn.png"/><preview>52ht3dd60csd</preview><watermark id="03wpxhco8js6" thumbnail="${
-	process.env.WATERMARKS_FOLDER}/Go!Animate.png"/><preview>03wpxhco8js6</preview>${
+	process.env.WATERMARKS_FOLDER}/GoMakeYourOwn.png"/><preview>52ht3dd60csd</preview>${
 	// custom watermarks
 	files.map(v => `<watermark id="${v.name}" thumbnail="${
 		  process.env.WATERMARKS_FOLDER}/${v.id}"/><preview>${v.id}</preview>`).join("")}</watermarks>`;
