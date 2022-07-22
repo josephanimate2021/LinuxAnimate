ef125f59f798a67f030eced6c242112e744c40a1
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-22 14:04:02 -0400

    Update flash.js

--- watermark/flash.js
+++ watermark/flash.js
@@ -2,11 +2,7 @@ module.exports = function (req, res) {
 	if (req.method != "GET") return;
 	const match = req.url.match(/\/_WATERMARKS\/(?:\.swf)?$/);
 	if (!match) return;
-	
-	var ext = match[1];
-	if (ext == "swf") {
-		res.setHeader("Content-Type", "application/x-shockwave-flash");
-		res.statusCode = 302;
-		res.end();
-	}
+	res.setHeader("Content-Type", "application/x-shockwave-flash");
+	res.statusCode = 302;
+	res.end();
 }
