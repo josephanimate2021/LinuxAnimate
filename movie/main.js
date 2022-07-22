fcb8d814ca89aeb37a88b2350d92739f2fe85f84
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-22 17:11:08 -0400

    Update main.js

--- movie/main.js
+++ movie/main.js
@@ -275,11 +275,11 @@ module.exports = {
 			const endTitle = buffer.indexOf("]]></title>");
 			const subtitle = buffer.slice(begTitle, endTitle).toString().trim();
 			
-			const begDesc = buffer.indexOf("<desc>") + 16;
+			const begDesc = buffer.indexOf("<desc>") + 1;
 			const endDesc = buffer.indexOf("]]></desc>");
 			const desc = buffer.slice(begDesc, endDesc).toString().trim();
 			
-			const begTag = buffer.indexOf("<tag>") + 16;
+			const begTag = buffer.indexOf("<tag>") + 1;
 			const endTag = buffer.indexOf("]]></tag>");
 			const subtag = buffer.slice(begTag, endTag).toString().trim();
 			var title, tag;
