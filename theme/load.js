4d8c9172b9e7bce3b23bbae74c1aeafa022d055f
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-22 17:38:22 -0400

    Update load.js

--- theme/load.js
+++ theme/load.js
@@ -1,13 +1,14 @@
-const loadPost = require('./postBody');
+const loadPost = require("../misc/post_body");
 const folder = process.env.THEME_FOLDER;
-const fUtil = require('../misc/file');
+const fs = require("fs");
+const http = require("http");
 
 module.exports = function (req, res, url) {
-	if (req.method != 'POST' || url.path != '/goapi/getTheme/') return;
-	loadPost(req, res).then(data => {
-		var theme = data.themeId;
-		res.setHeader('Content-Type', 'application/zip');
-		fUtil.zippy(`${folder}/${theme}.xml`, 'theme.xml').then(b => res.end(b));
+	if (req.method != "POST" || url.path != "/goapi/getTheme/") return; 
+	loadPost(req, res).then(([data]) => {
+		res.setHeader("Content-Type", "text/html; charset=UTF-8");
+		const p = `${folder}/${data.themeId}.xml`;
+		fs.createReadStream(p).pipe(res);
 	});
 	return true;
-}
+};
