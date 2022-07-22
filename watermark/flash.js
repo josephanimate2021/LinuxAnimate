22264e2c68d05884657c6ca1a9733d113aca6f4d
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-22 14:00:57 -0400

    Update flash.js

--- watermark/flash.js
+++ watermark/flash.js
@@ -1,40 +1,12 @@
 module.exports = function (req, res, url) {
-	switch (req.method) {
-		case "GET": {
-			const match = req.url.match(/\/movies\/([^.]+)(?:\.(zip|xml))?$/);
-			if (!match) return;
-
-			var id = match[1];
-			var ext = match[2];
-			switch (ext) {
-				case "zip":
-					res.setHeader("Content-Type", "application/zip");
-					movie.loadZip(id).then((v) => {
-						if (v) {
-							res.statusCode = 200;
-							res.end(v);
-						} else {
-							res.statusCode = 404;
-							res.end();
-						}
-					}).catch(e => {
-						console.log(e);
-					});
-					break;
-				default:
-					res.setHeader("Content-Type", "text/xml");
-					movie.loadXml(id).then((v) => {
-						if (v) {
-							res.statusCode = 200;
-							res.end(v);
-						} else {
-							res.statusCode = 404;
-							res.end();
-						}
-					}).catch(e => {
-						console.log(e);
-					});
-					break;
-			}
-			return true;
-		}
+	if (req.method != "GET") return;
+	const match = req.url.match(/\/_WATERMARKS\/(?:\.swf)?$/);
+	if (!match) return;
+	
+	var ext = match[1];
+	if (ext == "swf") {
+		res.setHeader("Content-Type", "application/zip");
+		res.statusCode = 302;
+		res.end();
+	}
+}
