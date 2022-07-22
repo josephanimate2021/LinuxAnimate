b8b3904ecfbf1ad3b088d4ba9cb5417b3190fcf5
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-22 11:51:20 -0400

    Update main.js

--- watermark/main.js
+++ watermark/main.js
@@ -3,68 +3,69 @@ const savedFolder = process.env.SAVED_FOLDER;
 const fs = require("fs");
 
 module.exports = {
-	save(mId, wId, wImg) {
+	save(mId, wId, wTitle) {
 		// vars
 		var path = `${folder}/${mId}.xml`;
 		var wXml; 
-		// custom watermarks
-		if (wImg) {
-			wXml = `<watermarks><watermark>/${folder}/${wImg}</watermark></watermarks>`;
-		} else {
+		// save watermarks
+		switch (wId) {
 			// premade watermarks
-			switch (wId) {
-				case "0dhteqDBt5nY": {
-					wXml = '<watermarks><watermark style="visualplugin"/></watermarks>';
-					break;
-				}
-				case "0vTLbQy9hG7k": {
-					wXml = '<watermarks></watermarks>';
-					break;
-				}
-				case "174tbqdo0cs6": {
-					wXml = '<watermarks><watermark style="g4s"/></watermarks>';
-					break;
-				}
-				case "52ht3dd60csd": {
-					wXml = '<watermarks><watermark style="twoLines"/></watermarks>';
-					break;
-				}
-				case "82tkgqdefbw6": {
-					wXml = '<watermarks><watermark style="freeTrial"/></watermarks>';
-					break;
-				}
+			case "0dhteqDBt5nY": {
+				wXml = '<watermarks><watermark style="visualplugin"/></watermarks>';
+				break;
+			}
+			case "0vTLbQy9hG7k": {
+				wXml = '<watermarks></watermarks>';
+				break;
+			}
+			case "174tbqdo0cs6": {
+				wXml = '<watermarks><watermark style="g4s"/></watermarks>';
+				break;
+			}
+			case "52ht3dd60csd": {
+				wXml = '<watermarks><watermark style="twoLines"/></watermarks>';
+				break;
+			}
+			case "82tkgqdefbw6": {
+				wXml = '<watermarks><watermark style="freeTrial"/></watermarks>';
+				break;
+			}
+			// custom watermarks
+			default: {
+				wXml = `<watermarks><watermark>/${folder}/${wId}-wtr.swf</watermark></watermarks>`;
+				break;
 			}
 		}
 		fs.writeFileSync(path, wXml);
-		this.assign(mId, wId, wImg);
+		this.assign(mId, wId);
 	},
-	assign(mId, wId, wImg) {
+	assign(mId, wId) {
 		var path = `${savedFolder}/${mId}.xml`;
 		var wXml;
-		if (wImg) {
-			wXml = '<watermark>Custom Logo<watermark>';
-		} else {
-			switch (wId) {
-				case "0dhteqDBt5nY": {
-					wXml = '<watermark>No Logo</watermark>';
-					break;
-				}
-				case "0vTLbQy9hG7k": {
-					wXml = '<watermark>GoAnimate Logo<watermark>';
-					break;
-				}
-				case "174tbqdo0cs6": {
-					wXml = '<watermark>GoAnimate For Schools Logo<watermark>';
-					break;
-				}
-				case "52ht3dd60csd": {
-					wXml = '<watermark>GoAnimate - Go Make Your Own Logo<watermark>';
-					break;
-				}
-				case "82tkgqdefbw6": {
-					wXml = '<watermark>GoAnimate Free Trial Logo<watermark>';
-					break;
-				}
+		switch (wId) {
+			case "0dhteqDBt5nY": {
+				wXml = '<watermark>No Logo</watermark>';
+				break;
+			}
+			case "0vTLbQy9hG7k": {
+				wXml = '<watermark>GoAnimate Logo<watermark>';
+				break;
+			}
+			case "174tbqdo0cs6": {
+				wXml = '<watermark>GoAnimate For Schools Logo<watermark>';
+				break;
+			}
+			case "52ht3dd60csd": {
+				wXml = '<watermark>GoAnimate - Go Make Your Own Logo<watermark>';
+				break;
+			}
+			case "82tkgqdefbw6": {
+				wXml = '<watermark>GoAnimate Free Trial Logo<watermark>';
+				break;
+			}
+			default: {
+				wXml = '<watermark>Custom Logo<watermark>';
+				break;
 			}
 		}
 		fs.writeFileSync(path, wXml);
