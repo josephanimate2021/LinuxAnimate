19242caac9fe83c636b1a0808f6bde8b5e19bc58
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-22 01:38:07 -0400

    Update watermark.js

--- asset/watermark.js
+++ asset/watermark.js
@@ -4,7 +4,15 @@ const base = Buffer.alloc(1, 0);
 
 async function listAssets(data) {
 	var files = asset.list(data.movieId, "wtr");
-	var xmlString = `<?xml encoding="UTF-8"?><watermarks><watermark id="174tbqdo0cs6" thumbnail="${process.env.WATERMARKS_FOLDER}/Go4Schools.png" name="GoAnimate For Schools Logo"/><preview>174tbqdo0cs6</preview><watermark id="82tkgqdefbw6" thumbnail="${process.env.WATERMARKS_FOLDER}/freeTrial.png" name="GoAnimate Free Trial Logo"/><preview>82tkgqdefbw6</preview><watermark id="52ht3dd60csd" thumbnail="${process.env.WATERMARKS_FOLDER}/GoMakeYourOwn.png" name="GoAnimate - Go Make Your Own Logo"/><preview>52ht3dd60csd</preview>${files.map(v => `<watermark id="${v.name}" thumbnail="${process.env.WATERMARKS_FOLDER}/${v.id}" name="Custom Logo"/><preview>${v.id}</preview>`).join("")}</watermarks>`;
+	// premade watermarks
+	var xmlString = `
+	<?xml encoding="UTF-8"?><watermarks><watermark id="174tbqdo0cs6" thumbnail="${
+	process.env.WATERMARKS_FOLDER}/Go4Schools.png"/><preview>174tbqdo0cs6</preview><watermark id="82tkgqdefbw6" thumbnail="${
+	process.env.WATERMARKS_FOLDER}/freeTrial.png"/><preview>82tkgqdefbw6</preview><watermark id="52ht3dd60csd" thumbnail="${
+	process.env.WATERMARKS_FOLDER}/GoMakeYourOwn.png"/><preview>52ht3dd60csd</preview>${
+	// custom watermarks
+	files.map(v => `<watermark id="${v.name}" thumbnail="${
+		  process.env.WATERMARKS_FOLDER}/${v.id}"/><preview>${v.id}</preview>`).join("")}</watermarks>`;
 	return xmlString;
 }
 
@@ -17,7 +25,6 @@ module.exports = function (req, res, url) {
 	}
 	loadPost(req, res).then(([data]) => listAssets(data)).then((buff) => {
 		res.setHeader("Content-Type", "text/xml");
-		console.log(buff);
 		res.end(buff);
 	});
 	return true;
