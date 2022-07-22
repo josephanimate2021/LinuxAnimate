541a263a963d47bc0cbecc42d5c59a29f7ea0795
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-22 11:53:44 -0400

    Update watermark.js

--- movie/watermark.js
+++ movie/watermark.js
@@ -5,23 +5,7 @@ module.exports = function (req, res, url) {
   if (!match) return;
 
   const mId = match[1];
-  var wId = match[2];
-  var id, wImg;
-  switch (wId) {
-    case "0vTLbQy9hG7k":
-    case "174tbqdo0cs6":
-    case "52ht3dd60csd":
-    case "82tkgqdefbw6":
-    case "0dhteqDBt5nY": {
-      id = watermark.save(mId, wId);
-      res.end(id);
-      break;
-    }
-    default: {
-      wImg = match[2];
-      wId = wImg.slice(0, -8);
-      id = watermark.save(mId, wId, wImg);
-      res.end(id);
-    }
-  }
+  const wId = match[2];
+  const id = watermark.save(mId, wId);
+  res.end(id);
 }
