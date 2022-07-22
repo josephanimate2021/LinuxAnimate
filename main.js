45a3b8a6333e90751d15cf71f74ec7966a248b6a
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-22 14:39:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -9,17 +9,15 @@ const env = Object.assign(process.env, require("./env"), require("./config"));
 
 // folders
 const forms = env.WAVEFORMS_FOLDER;
-const wtr = env.WATERMARKS_FOLDER;
-const assets = env.PROPS_FOLDER;
+const asset = env.PROPS_FOLDER;
 const cache = env.CACHÉ_FOLDER;
 const saved = env.SAVED_FOLDER;
 
 // create directories if they're missing
-if (!fs.existsSync(assets)) fs.mkdirSync(assets);
+if (!fs.existsSync(asset)) fs.mkdirSync(asset);
 if (!fs.existsSync(cache)) fs.mkdirSync(cache);
 if (!fs.existsSync(saved)) fs.mkdirSync(saved);
 if (!fs.existsSync(forms)) fs.mkdirSync(forms);
-if (!fs.existsSync(wtr)) fs.mkdirSync(wtr);
 
 // start server
 server();
