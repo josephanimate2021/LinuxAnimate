8e4f3ed247d025a695d90ede9af545800ccad6ff
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-22 23:39:47 -0400

    Update load.js

--- tts/load.js
+++ tts/load.js
@@ -478,26 +478,22 @@ function tts(voiceName, text) {
  * @returns {boolean}
  */
 module.exports = function (req, res, url) {
-	if (req.method != "POST" || url.path != "/goapi/convertTextToSoundAsset/" || url.path != "/goapi/getAssetEx/") return;
-	loadPost(req, res).then(([data, mId]) => {
-		tts(data.voice, data.text)
-			.then((buffer) => {
-				mp3Duration(buffer, (e, d) => {
-					var dur = d * 1e3;
-					if (e || !dur) {
-						return res.end(1 + util.xmlFail("Unable to retrieve MP3 stream."));
-					}
+	if (req.method != "POST" || url.path != "/goapi/convertTextToSoundAsset/") return;
+	loadPost(req, res).then(data => {
+		tts(data.voice, data.text).then(buffer => {
+			mp3Duration(buffer, (e, d) => {
+				var dur = d * 1e3;
+				if (e || !dur) {
+					return res.end(1 + util.xmlFail("Unable to retrieve MP3 stream."));
+				}
 
-					const title = `[${voices[data.voice].desc}] ${data.text}`;
-					const id = asset.save(buffer, mId, "tts", "mp3");
-					res.end(
-						`0<response><asset><id>${id}</id><enc_asset_id>${id}</enc_asset_id><type>sound</type><subtype>tts</subtype><title>${title}</title><published>0</published><tags></tags><duration>${dur}</duration><downloadtype>progressive</downloadtype><file>${id}</file></asset></response>`
-					);
-				});
-			})
-			.catch((e) => {
-				res.end(1 + util.xmlFail(e));
+				const title = `[${voices[data.voice].desc}] ${data.text}`;
+				const id = asset.save(buffer, data.movieId, "tts", "mp3");
+				res.end(`0<response><asset><id>${id}</id><enc_asset_id>${id}</enc_asset_id><type>sound</type><subtype>tts</subtype><title>${title}</title><published>0</published><tags></tags><duration>${dur}</duration><downloadtype>progressive</downloadtype><file>${id}</file></asset></response>`);
 			});
+		}).catch(e => {
+			res.end(1 + util.xmlFail(e));
+		});
 	});
 	return true;
 };
