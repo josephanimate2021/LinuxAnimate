16feb686994ce7ad63d6809e7dd107d55ab30e7b
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-08-08 18:20:12 -0400

    Update premade.js

--- character/premade.js
+++ character/premade.js
@@ -1,5 +1,18 @@
+const loadPost = require("../misc/post_body");
+const fs = require("fs");
+const folder = process.env.STARTERS_FOLDER;
 module.exports = function (req, res, url) {
-	if (req.method != "POST" || url.path != "/goapi/getCCPreMadeCharacters") return; 
-	res.end();
-	return true;
+	if (req.method != "POST") return;
+	switch (url.pathname) { 
+		case "/goapi/getCCPreMadeCharacters": {
+			res.end();
+			return true;
+		} case "/goapi/getSysTemplates/": {
+			loadPost(req, res).then(([data]) => {
+				const buffer = fs.readFileSync(`${folder}/${data.themeId}.xml`);
+				res.end(buffer);
+			});
+			return true;
+		} default: return;
+	}
 };
