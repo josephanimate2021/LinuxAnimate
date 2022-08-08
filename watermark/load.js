1f1cad120ed0375a7227d1ced2bb547f4349f42c
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-08-08 16:48:30 -0400

    Update load.js

--- watermark/load.js
+++ watermark/load.js
@@ -10,7 +10,7 @@ module.exports = function (req, res, url) {
 		const p = `${folder}/${data.movieId}.xml`;
 		if (!fs.existsSync(p)) {
 			// don't load any watermarks if a user watermark does not exist.
-			res.end('<watermarks><watermark style="visualplugin"/></watermarks>');
+			res.end('<movie owner="NederlandComedian" id="0Ndt5hDY9FwM"/><watermarks><watermark style="visualplugin"/></watermarks>');
 		} else {
 			fs.createReadStream(p).pipe(res);
 		}
