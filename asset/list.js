8cc8a2207931b0f26f002e7a5d085d15e26989a5
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-08-08 16:17:30 -0400

    Update list.js

--- asset/list.js
+++ asset/list.js
@@ -1,5 +1,4 @@
 const loadPost = require("../misc/post_body");
-const header = process.env.XML_HEADER;
 const fUtil = require("../misc/file");
 const nodezip = require("node-zip");
 const movie = require("../movie/main");
@@ -14,7 +13,7 @@ async function listAssets(data, makeZip) {
 	switch (data.type) {
 		case "char": {
 			const chars = await asset.chars(data.themeId);
-			xmlString = `${header}<ugc more="0">${chars
+			xmlString = `<ugc more="0">${chars
 				.map(
 					(v) =>
 						`<char id="${v.id}" name="Untitled" cc_theme_id="${v.theme}" thumbnail_url="/char_thumbs/${v.id}.png" copyable="Y"><tags/></char>`
@@ -24,21 +23,21 @@ async function listAssets(data, makeZip) {
 		}
 		case "bg": {
 			files = asset.list(mId, "bg");
-			xmlString = `${header}<ugc more="0">${files
+			xmlString = `<ugc more="0">${files
 				.map((v) => `<background subtype="0" id="${v.id}" name="${v.name}" enable="Y"/>`)
 				.join("")}</ugc>`;
 			break;
 		}
 		case "sound": {
 			files = asset.list(mId, "sound");
-			xmlString = `${header}<ugc more="0">${files
+			xmlString = `<ugc more="0">${files
 				.map((v) =>`<sound subtype="${v.subtype}" id="${v.id}" name="${v.name}" enable="Y" duration="${v.duration}" downloadtype="progressive"/>`)
 				.join("")}</ugc>`;
 			break;
 		}
 		case "movie": {
 			files = movie.listStarters();
-			xmlString = `${header}<ugc more="0">${files
+			xmlString = `<ugc more="0">${files
 				.map(
 					(v) =>
 						`<movie id="${v.id}" enc_asset_id="${v.id}" path="/_SAVED/${v.id}" numScene="1" title="Untitled" thumbnail_url="/starter_thumbs/${v.id}.png"><tags></tags></movie>`
@@ -48,7 +47,7 @@ async function listAssets(data, makeZip) {
 		}
 		case "prop": {
 			files = asset.list(mId, "prop");
-			xmlString = `${header}<ugc more="0">${files
+			xmlString = `<ugc more="0">${files
 				.map(
 				        (v) =>
 						`<prop subtype="0" id="${v.id}" name="${v.name}" enable="Y" holdable="0" headable="0" placeable="1" facing="left" width="0" height="0" asset_url="${process.env.PROPS_FOLDER}/${v.id}"/>`
@@ -57,7 +56,7 @@ async function listAssets(data, makeZip) {
 			break;
 		}
 		default: {
-			xmlString = `${header}<ugc more="0"></ugc>`;
+			xmlString = `<ugc more="0"></ugc>`;
 			break;
 		}
 	}
@@ -65,7 +64,7 @@ async function listAssets(data, makeZip) {
 	switch (data.subtype) {
 		case "video": {
 			files = asset.list(mId, "video");
-			xmlString = `${header}<ugc more="0">${files
+			xmlString = `<ugc more="0">${files
 				.map(
 					(v) =>
 						`<prop subtype="video" id="${v.id}" name="${v.name}" enable="Y" holdable="0" headable="0" placeable="1" facing="left" width="10" height="10" thumbnail_url=""/>`
