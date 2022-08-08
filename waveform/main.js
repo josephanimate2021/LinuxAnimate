ca85a554f73db3f0f36e0b6638794e8332665847
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-08-08 13:42:58 -0400

    Update main.js

--- waveform/main.js
+++ waveform/main.js
@@ -2,8 +2,8 @@ const folder = process.env.WAVEFORMS_FOLDER;
 const fs = require("fs");
 
 module.exports = {
-	save(wId) {
-		var path = `${folder}/${wId}.wf`;
-		fs.writeFileSync(path);
+	save(wfId, waveform) {
+		var path = `${folder}/${wfId}.wf`;
+		fs.writeFileSync(path, waveform);
 	}
 };
