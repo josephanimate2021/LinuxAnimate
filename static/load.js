589be6c5589171544ad967ba266eb49993924e3c
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-08-09 19:35:39 -0400

    Update load.js

--- static/load.js
+++ static/load.js
@@ -38,6 +38,7 @@ module.exports = function (req, res, url) {
 							content = content.replace(/NEWS/g, "The First Half Of LinuxAnimate Development Is Complete. Pretty soon, updating asset names, tags, and theme searching are to await. if this is your first time using linuxanimate and you are not in the goanimate community, please click");
 						}
 						content = content.replace(/LINK/g, `<a href="https://josephanimate2021.github.io/lvm-static/2014?api=${apiPath}&action=create&tutorial=0&tray=retro">here</a>`);
+						content = content.replace(/BUTTON/g, `<a class="button_big" href="https://josephanimate2021.github.io/lvm-static/2014?api=${apiPath}&action=create&tray=retro">MAKE A VIDEO</a>`);
 						res.end(content);
 					} else {
 						fs.createReadStream(path).pipe(res);
