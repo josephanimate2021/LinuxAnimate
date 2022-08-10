3be0566f54e9960072f44386176e8e1af76cffa1
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-08-09 21:20:49 -0400

    Update load.js

--- tts/load.js
+++ tts/load.js
@@ -479,25 +479,19 @@ function tts(voiceName, text) {
 module.exports = function (req, res, url) {
 	if (req.method != "POST" || url.path != "/goapi/convertTextToSoundAsset/") return;
 	loadPost(req, res).then(([data]) => {
-		const mId = data.ut;
-		tts(data.voice, data.text)
-			.then((buffer) => {
-				mp3Duration(buffer, (e, d) => {
-					var dur = d * 1e3;
-					if (e || !dur) {
-						return res.end(1 + util.xmlFail("Unable to retrieve MP3 stream."));
-					}
-
-					const title = `[${voices[data.voice].desc}] ${data.text}`;
-					const id = asset.save(buffer, mId, "tts", "mp3");
-					res.end(
-						`0<response><asset><id>${id}</id><enc_asset_id>${id}</enc_asset_id><type>sound</type><subtype>tts</subtype><title>${title}</title><published>0</published><tags></tags><duration>${dur}</duration><downloadtype>progressive</downloadtype><file>${id}</file></asset></response>`
-					);
-				});
-			})
-			.catch((e) => {
-				res.end(1 + util.xmlFail(e));
+		const ut = data.ut;
+		tts(data.voice, data.text).then((buffer) => {
+			mp3Duration(buffer, (e, d) => {
+				var dur = d * 1e3;
+				if (e || !dur) return res.end(1 + util.xmlFail("Unable to retrieve MP3 stream."));
+				const title = `[${voices[data.voice].desc}] ${data.text}`;
+				const id = asset.save(buffer, ut, "tts", "mp3");
+				res.end(`0<response><asset><id>${id}</id><enc_asset_id>${id}</enc_asset_id><type>sound</type><subtype>tts</subtype><title>${title}</title><published>0</published><tags></tags><duration>${dur}</duration><downloadtype>progressive</downloadtype><file>${id}</file></asset></response>`);
 			});
+		}).catch(e => {
+			console.log("Error:", e);
+			return res.end(1 + util.xmlFail(e));
+		});
 	});
 	return true;
 };
