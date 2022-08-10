2653ab8b2840b363ef35d54feddb5fb755761388
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-08-09 23:11:24 -0400

    Update meta.js

--- movie/meta.js
+++ movie/meta.js
@@ -9,7 +9,8 @@ const http = require("http");
  */
 module.exports = function (req, res, url) {
 	if (req.method != "GET" || !url.pathname.startsWith("/meta")) return;
-	movie.meta(url.path.substr(url.path.lastIndexOf("/") + 1)).then((v) => res.end(JSON.stringify(v))).catch(e => {
+	const headers = req.headers.host;
+	movie.meta(headers, url.path.substr(url.path.lastIndexOf("/") + 1)).then((v) => res.end(JSON.stringify(v))).catch(e => {
 		res.statusCode = 404;
 		console.log("Error:", e);
 	});
