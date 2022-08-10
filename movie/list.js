b6a9558f7f81c0648bab8dc9762e4b9eb7b85fd0
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-08-09 23:24:11 -0400

    Update list.js

--- movie/list.js
+++ movie/list.js
@@ -10,6 +10,7 @@ const http = require("http");
 module.exports = function (req, res, url) {
 	if (req.method != "GET" || url.pathname != "/movieList") return;
 	const headers = req.headers.host;
-	Promise.all(movie.list().map(movie.meta)).then((a) => res.end(JSON.stringify(a)));
+	const meta = movie.meta(headers);
+	Promise.all(movie.list().map(meta)).then((a) => res.end(JSON.stringify(a)));
 	return true;
 };
