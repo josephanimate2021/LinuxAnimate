0d50df1560893be89f4bfb3bda402cc94adeb06d
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-08-09 20:43:23 -0400

    Error catcher for assets

--- asset/load.js
+++ asset/load.js
@@ -21,75 +21,69 @@ module.exports = function (req, res, url) {
 			const aId = match[2];
 			const b = asset.load(ut, aId);
 			if (b) {
-				res.statusCode = 200;
-				res.end(b);
-			} else {
-				res.statusCode = 404;
-				res.end(e);
+				try {
+					res.statusCode = 200;
+					res.end(b);
+				} catch (e) {
+					res.statusCode = 404;
+					console.log("Error:", e);
+				}
 			}
-			const charMatch = req.url.match(/\/characters\/([^.]+)(?:\.xml)?$/);
-			if (!charMatch) return;
-
-			var id = charMatch[1];
-			res.setHeader("Content-Type", "text/xml");
-			character
-				.load(id)
-				.then((v) => {
-					(res.statusCode = 200), res.end(v);
-				})
-				.catch((e) => {
-					(res.statusCode = 404), res.end(e);
-				});
-			return true;
 		}
 
 		case "POST": {
 			switch (url.pathname) {
 				case "/goapi/getAsset/":
 				case "/goapi/getAssetEx/": {
-					loadPost(req, res).then(([data]) => {
-						const aId = data.assetId || data.enc_asset_id;
-						console.log(data.ut);
-						const b = asset.load(data.ut, aId);
-						if (b) {
-							res.setHeader("Content-Length", b.length);
-							res.setHeader("Content-Type", "audio/mp3");
-							res.end(b);
-						} else {
-							res.statusCode = 404;
-							res.end();
-						}
-					});
+					try {
+						loadPost(req, res).then(([data]) => {
+							const aId = data.assetId || data.enc_asset_id;
+							const b = asset.load(data.ut, aId);
+							if (b) {
+								res.setHeader("Content-Length", b.length);
+								res.setHeader("Content-Type", "audio/mp3");
+								res.end(b);
+							}
+						});
+					} catch (e) {
+						console.log("Error:", e);
+					}
 					return true;
 				}
 				case "/goapi/deleteAsset/": {
-					// only accept the request on localhost
-					switch (req.headers.host) {
-						case "localhost": 
-						case `localhost:${process.env.SERVER_PORT}`: {
-							loadPost(req, res).then(async ([data]) => {
-								const ut = data.ut;
-								const aId = data.assetId || data.enc_asset_id;
-								asset.delete(ut, aId);
-							});
-							return true;
+					try {
+						// only accept the request on localhost
+						switch (req.headers.host) {
+							case "localhost": 
+							case `localhost:${process.env.SERVER_PORT}`: {
+								loadPost(req, res).then(async ([data]) => {
+									const ut = data.ut;
+									const aId = data.assetId || data.enc_asset_id;
+									asset.delete(ut, aId);
+								});
+								return true;
+							}
 						}
+					} catch (e) {
+						console.log("Error:", e);
 					}
 				}
 				case "/goapi/DeleteUserTemplate/": {
 					const port = process.env.SERVER_PORT;
 					// only accept the request on localhost
-					switch (req.headers.host) {
-						case "localhost": loadPost(req, res).then(async ([data]) => starter.delete(data.templateId));
-						case `localhost:${port}`: loadPost(req, res).then(async ([data]) => starter.delete(data.templateId));
+					try {
+						switch (req.headers.host) {
+							case "localhost": loadPost(req, res).then(async ([data]) => starter.delete(data.templateId));
+							case `localhost:${port}`: loadPost(req, res).then(async ([data]) => starter.delete(data.templateId));
+						}
+					} catch (e) {
+						console.log("Error:", e);
 					}
 					return true;
 				}
-				default:
-					return;
+				default: return;
 			}
 		}
-		default:
-			return;
+		default: return;
 	}
 };
