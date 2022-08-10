a628dfb2ef6d6981f7f46ed2766d95168051572f
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-08-09 21:07:02 -0400

    Update watermark.js

--- movie/watermark.js
+++ movie/watermark.js
@@ -3,9 +3,12 @@ module.exports = function (req, res, url) {
   if (req.method != "POST") return;
   const match = req.url.match(/\/goapi\/assignwatermark\/movie\/([^/]+)\/([^.]+)$/);
   if (!match) return;
-
-  const mId = match[1];
-  const wId = match[2];
-  const id = watermark.save(mId, wId);
-  res.end(id);
+  try {
+    const mId = match[1];
+    const wId = match[2];
+    const id = watermark.save(mId, wId);
+    res.end(id);
+  } catch (e) {
+    console.log("Error:", e);
+  }
 }
