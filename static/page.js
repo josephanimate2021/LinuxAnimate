8f6ac385afeec3d6e8de84c7cf85efaac12850db
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-08-10 00:12:57 -0400

    Update page.js

--- static/page.js
+++ static/page.js
@@ -113,46 +113,6 @@ module.exports = function (req, res, url) {
 			};
 			break;
 		}
-			
-		case "/go_full": {
-			let presave =
-				query.movieId && query.movieId.startsWith("m")
-					? query.movieId
-					: `m-${fUtil[query.noAutosave ? "getNextFileId" : "fillNextFileId"]("movie-", ".xml")}`;
-			title = "Video Editor";
-			attrs = {
-				data: process.env.SWF_URL + "/go_full.swf",
-				type: "application/x-shockwave-flash",
-				width: "100%",
-				height: "100%",
-			};
-			params = {
-				flashvars: {
-					apiserver: "/",
-					storePath: process.env.STORE_URL + "/<store>",
-					isEmbed: 1,
-					ctc: "go",
-					ut: ut,
-					tutorialReload: false,
-					appCode: "go",
-					page: "",
-					siteId: "go",
-					lid: 13,
-					isLogin: "Y",
-					retut: 1,
-					clientThemePath: process.env.CLIENT_URL + "/<client_theme>",
-					tlang: "en_US",
-					presaveId: presave,
-					goteam_draft_only: 1,
-					isWide: 1,
-					collab: 0,
-					nextUrl: "/html/list.html",
-					tray: trayQuery,
-				},
-				allowScriptAccess: "always",
-			};
-			break;
-		}
 
 		case "/player": {
 			title = "Player";
