647af7e93a8a43632806a1a3f1314ddd72d43818
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-08-09 22:44:30 -0400

    Update load.js

--- static/load.js
+++ static/load.js
@@ -41,48 +41,46 @@ module.exports = function (req, res, url) {
 						}
 						content = content.replace(/LINK/g, `<a href="https://josephanimate2021.github.io/lvm-static/2014?api=${apiPath}&action=create&tutorial=0&tray=retro&return=${apiPath}&platform=${platform}">here</a>`);
 						content = content.replace(/BUTTON/g, `<a class="button_big" href="https://josephanimate2021.github.io/lvm-static/2014?api=${apiPath}&action=create&tray=retro&return=${apiPath}&platform=${platform}">MAKE A VIDEO</a>`);
-						content = content.replace(/SCRIPT/g, `<script>
-		var json;
-		var tbody = document.getElementsByTagName('tbody')[0];
-		var loadMore = document.getElementById('load_more');
-		const listReq = new XMLHttpRequest();
-		listReq.open('GET', '/movieList');
-		listReq.send();
+						content = content.replace(/SCRIPT/g, '<script>var json;\
+		var tbody = document.getElementsByTagName(\'tbody\')[0];\
+		var loadMore = document.getElementById(\'load_more\');\
+		const listReq = new XMLHttpRequest();\
+		listReq.open(\'GET\', \'/movieList\');\
+		listReq.send();\
 
-		var C = 0;
-		function loadRows() {
-			let c = C; C += 69;
-			for (; c < C; c++) {
-				if (c > json.length - 1) {
-					loadMore.remove();
-					break;
-				}
+		var C = 0;\
+		function loadRows() {\
+			let c = C; C += 69;\
+			for (; c < C; c++) {\
+				if (c > json.length - 1) {\
+					loadMore.remove();\
+					break;\
+				}\
 
-				const tbl = json[c];
-				const date = tbl.date.substr(0, 10) + ' ' + tbl.date.substr(11);
-				tbody.insertAdjacentHTML('beforeend',
-					'<tr id="' + tbl.id + '"><td><img src="/movie_thumbs/' + tbl.id + '"></td><td><div>' + tbl.title.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;") + '</div><div>' + tbl.durationString + '</div><div>' + tbl.desc + '</div><div>Video Tags: ' + tbl.tag + '</div><div>Watermark: ' + tbl.watermark + '</div><div>Video Res: ' + tbl.res + '</td><td><span>' + date + '</span></td><td><a href="javascript:;" onclick="popup(\'' + tbl.id + '\', \'' + tbl.wide + '\')"></a><a href="https://josephanimate2021.github.io/lvm-static/2014?api=${apiPath}&action=edit&movieId=' + tbl.id + '&isWide=' + tbl.wide + '"></a><a href="/movies/' + tbl.id + '.xml" download="' + tbl.title + '"></a><a href="javascript:deleteMovie(\'' + tbl.id + '\')"></a></td></tr>');
-			}
-		}
+				const tbl = json[c];\
+				const date = tbl.date.substr(0, 10) + ' ' + tbl.date.substr(11);\
+				tbody.insertAdjacentHTML(\'beforeend\',\
+					\'<tr id="\' + tbl.id + \'"><td><img src="/movie_thumbs/\' + tbl.id + \'"></td><td><div>\' + tbl.title.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;") + \'</div><div>\' + tbl.durationString + \'</div><div>\' + tbl.desc + \'</div><div>Video Tags: \' + tbl.tag + \'</div><div>Watermark: \' + tbl.watermark + \'</div><div>Video Res: \' + tbl.res + \'</td><td><span>\' + date + \'</span></td><td><a href="javascript:;" onclick="popup(\'\' + tbl.id + \'\', \'\' + tbl.wide + \'\')"></a><a href="https://josephanimate2021.github.io/lvm-static/2014?api=' + apiPath + '&action=create&movieId=\' + tbl.id + \'&isWide=\' + tbl.wide + \'"></a><a href="/movies/\' + tbl.id + \'.xml" download="\' + tbl.title + \'"></a><a href="javascript:deleteMovie(\'\' + tbl.id + \'\')"></a></td></tr>\');\
+			}\
+		}\
 
-		loadMore.onclick = loadRows;
-		listReq.onreadystatechange = function (e) {
-			if (listReq.readyState != 4) return;
-			json = JSON.parse(listReq.responseText);
-			loadRows();
-		}
+		loadMore.onclick = loadRows;\
+		listReq.onreadystatechange = function (e) {\
+			if (listReq.readyState != 4) return;\
+			json = JSON.parse(listReq.responseText);\
+			loadRows();\
+		}\
 
-		function popup(id, wide) {
-			window.open('/player?movieId=' + id + '&isWide=' + wide, 'MsgWindow', 'width=1280,height=723,left=' + (screen.width / 2 - 640) + ',top=' + (screen.height / 2 - 360));
-		}
-		
-		function deleteMovie(id) {
-			const xhttp = new XMLHttpRequest();
-		        xhttp.open('POST', '/ajax/deleteMovie/?movieId=' + id);
-		        xhttp.send();
-                        document.getElementById(id).style.display = "none";
-		}
-	</script>`);
+		function popup(id, wide) {\
+			window.open(\'/player?movieId=\' + id + \'&isWide=\' + wide, \'MsgWindow\', \'width=1280,height=723,left=\' + (screen.width / 2 - 640) + \',top=\' + (screen.height / 2 - 360));\
+		}\
+
+		function deleteMovie(id) {\
+			const xhttp = new XMLHttpRequest();\
+		        xhttp.open(\'POST\', `/ajax/deleteMovie/?movieId=${id}`);\
+		        xhttp.send();\
+                        document.getElementById(`${id}`).style.display = "none";\
+		});</script>');
 						res.end(content);
 					} else {
 						fs.createReadStream(path).pipe(res);
