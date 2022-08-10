422cd2809ad5c6c2f51bfd0e5bdc3168d9588936
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-08-09 20:57:27 -0400

    Update save.js

--- character/save.js
+++ character/save.js
@@ -9,15 +9,13 @@ const http = require("http");
  * @returns {boolean}
  */
 module.exports = function (req, res, url) {
-	if (req.method == "POST")
-		switch (url.pathname) {
-			case "/goapi/saveCCCharacter/":
-				loadPost(req, res).then(([data]) => character.save(Buffer.from(data.body)).then((id) => {
-					var thumb = Buffer.from(data.thumbdata, "base64");
-					character.saveThumb(thumb, id);
-					res.end(`0${id}`);
-				}).catch(e => console.log("Error:", e)));
-				return true;
-		}
-	return;
+	if (req.method != "POST") return;
+	switch (url.pathname) {
+		case "/goapi/saveCCCharacter/": loadPost(req, res).then(([data]) => character.save(Buffer.from(data.body)).then((id) => {
+			var thumb = Buffer.from(data.thumbdata, "base64");
+			character.saveThumb(thumb, id);
+			res.end(`0${id}`);
+		}).catch(e => console.log("Error:", e)));
+			return true;
+	}
 };
