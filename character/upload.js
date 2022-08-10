303260d236846141b23319b470bdc74cdc42ba18
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-08-09 21:00:13 -0400

    Update upload.js

--- character/upload.js
+++ character/upload.js
@@ -12,20 +12,24 @@ const fs = require("fs");
  */
 module.exports = function (req, res, url) {
 	if (req.method != "POST" || url.path != "/upload_character") return;
-	new formidable.IncomingForm().parse(req, (e, f, files) => {
-		if (!files.import) return;
-		var path = files.import.path;
-		var buffer = fs.readFileSync(path);
-        const tIDbeg = buffer.indexOf('" theme_id="') + 12;
-		const tIDend = buffer.indexOf('" x="');
-		const themeId = buffer.subarray(tIDbeg, tIDend).toString();
-		var numId = fUtil.getNextFileId("char-", ".xml");
-		parse.unpackXml(buffer, `c-${numId}`);
-		fs.unlinkSync(path);
-		res.statusCode = 302;
-		var url = `/cc?themeId=${themeId}&original_asset_id=c-${numId}`;
-		res.setHeader("Location", url);
-		res.end();
-	});
+	try {
+		new formidable.IncomingForm().parse(req, (e, f, files) => {
+			if (!files.import) return;
+			var path = files.import.path;
+			var buffer = fs.readFileSync(path);
+			const tIDbeg = buffer.indexOf('" theme_id="') + 12;
+			const tIDend = buffer.indexOf('" x="');
+			const themeId = buffer.subarray(tIDbeg, tIDend).toString();
+			var numId = fUtil.getNextFileId("char-", ".xml");
+			parse.unpackCharXml(buffer, `c-${numId}`);
+			fs.unlinkSync(path);
+			res.statusCode = 302;
+			var url = `/cc?themeId=${themeId}&original_asset_id=c-${numId}`;
+			res.setHeader("Location", url);
+			res.end();
+		});
+	} catch (e) {
+		console.log("Error:", e);
+	}
 	return true;
 };
