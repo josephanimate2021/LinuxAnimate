9484bf3a92cf3c710a985c9412f8ec7d26d2560b
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-08-09 20:52:21 -0400

    Update load.js

--- character/load.js
+++ character/load.js
@@ -1,4 +1,5 @@
 const loadPost = require("../misc/post_body");
+const loadPost = require("../misc/util");
 const character = require("./main");
 const http = require("http");
 
@@ -16,14 +17,10 @@ module.exports = function (req, res) {
 
 			var id = match[1];
 			res.setHeader("Content-Type", "text/xml");
-			character
-				.load(id)
-				.then((v) => {
-					(res.statusCode = 200), res.end(v);
-				})
-				.catch((e) => {
-					(res.statusCode = 404), res.end(e);
-				});
+			character.load(id).then((v) => {
+				res.statusCode = 200;
+				res.end(v);
+			}).catch(e => console.log("Error:", e));
 			return true;
 		}
 
@@ -31,23 +28,18 @@ module.exports = function (req, res) {
 			if (req.url != "/goapi/getCcCharCompositionXml/") return;
 			loadPost(req, res).then(async ([data]) => {
 				res.setHeader("Content-Type", "text/html; charset=UTF-8");
-				character
-					.load(data.assetId || data.original_asset_id)
-					.then((v) => {
-						(res.statusCode = 200), res.end(0 + v);
-					})
-					//.catch(e => { res.statusCode = 404, res.end(1 + e) })
-
-					// Character not found?	Why not load my archnemesis instead?
-					.catch(() =>
-						character.load("a-306687427").then((v) => {
-							(res.statusCode = 200), res.end(0 + v);
-						})
-					);
+				character.load(data.assetId || data.original_asset_id).then((v) => {
+					res.statusCode = 200; 
+					res.end(0 + v);
+				}).catch(e => { 
+					res.statusCode = 404;
+					console.log("Error:", e);
+					// who knows. the lvm might need to return the error as well.
+					res.end(1 + util.xmlFail(e));
+				});
 			});
 			return true;
 		}
-		default:
-			return;
+		default: return;
 	}
 };
