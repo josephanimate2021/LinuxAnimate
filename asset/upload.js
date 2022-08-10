c29d7e3888d91790b930d4d0e118d0af89cff616
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-08-09 20:47:15 -0400

    Update upload.js

--- asset/upload.js
+++ asset/upload.js
@@ -14,27 +14,25 @@ module.exports = function (req, res, url) {
 	if (req.method != "POST") return;
 	switch (url.pathname) {
 		case "/upload_asset":
-			formidable().parse(req, (_, fields, files) => {
-				var [mId, mode, ext] = fields.params.split(".");
-				switch (mode) {
-					case "vo":
-						mode = "voiceover";
-						break;
-					case "se":
-						mode = "soundeffect";
-						break;
-					case "mu":
-						mode = "music";
-						break;
-				}
+			try {
+				formidable().parse(req, (_, fields, files) => {
+					var [mId, mode, ext] = fields.params.split(".");
+					switch (mode) {
+						case "vo": mode = "voiceover";
+						case "se": mode = "soundeffect";
+						case "mu": mode = "music";
+					}
 
-				var path = files.import.path;
-				var buffer = fs.readFileSync(path);
-				asset.save(buffer, mId, mode, ext);
-				fs.unlinkSync(path);
-				delete buffer;
-				res.end();
-			});
+					var path = files.import.path;
+					var buffer = fs.readFileSync(path);
+					asset.save(buffer, mId, mode, ext);
+					fs.unlinkSync(path);
+					delete buffer;
+					res.end();
+				});
+			} catch (e) {
+				console.log("Error:", e);
+			}
 			return true;
 	}
 };
