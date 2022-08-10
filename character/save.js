dd4a9de95d2810e03db722487fea51447a231f01
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-08-09 20:55:54 -0400

    Update save.js

--- character/save.js
+++ character/save.js
@@ -12,25 +12,11 @@ module.exports = function (req, res, url) {
 	if (req.method == "POST")
 		switch (url.pathname) {
 			case "/goapi/saveCCCharacter/":
-				loadPost(req, res).then(([data]) =>
-					character
-						.save(Buffer.from(data.body))
-						.then((id) => {
-							var thumb = Buffer.from(data.thumbdata, "base64");
-							character.saveThumb(thumb, id);
-							res.end(`0${id}`);
-						})
-						.catch(() => res.end(`10`))
-				);
-				return true;
-
-			case "/goapi/saveCCThumbs/":
-				loadPost(req, res).then(([data]) => {
-					var id = data.assetId;
+				loadPost(req, res).then(([data]) => character.save(Buffer.from(data.body)).then((id) => {
 					var thumb = Buffer.from(data.thumbdata, "base64");
 					character.saveThumb(thumb, id);
 					res.end(`0${id}`);
-				});
+				}).catch(e => console.log("Error:", e)));
 				return true;
 		}
 	return;
