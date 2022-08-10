85834a38ba701ac6d69db4975c999e7d688fc7b8
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-08-10 14:16:36 -0400

    Update upload.js

--- movie/upload.js
+++ movie/upload.js
@@ -15,36 +15,39 @@ module.exports = function (req, res, url) {
 	if (req.method != "POST") return;
 	switch (url.pathname) {
 		case "/upload_movie": {
-			new formidable.IncomingForm().parse(req, (e, f, files) => {
-				if (!files.import) return;
-				var path = files.import.path;
-				var buffer = fs.readFileSync(path);
-				const numId = fUtil.generateId();
-				switch (req.headers.host) {
-					case "localhost": 
-					case `localhost:${process.env.SERVER_PORT}`: {
-						parse.unpackXml(buffer, `${numId}`);
-						break;
+			try {
+				new formidable.IncomingForm().parse(req, (e, f, files) => {
+					if (!files.import) return;
+					var path = files.import.path;
+					var buffer = fs.readFileSync(path);
+					const numId = fUtil.generateId();
+					switch (req.headers.host) {
+						case "localhost": 
+						case `localhost:${process.env.SERVER_PORT}`: {
+							parse.unpackXml(buffer, `${numId}`);
+							break;
+						}
 					}
-				}
-				fs.unlinkSync(path);
-
-				res.statusCode = 302;
-				// why
-				var apiPath, url;
-				if (req.headers.host != "localhost" && req.headers.host != `localhost:${process.env.SERVER_PORT}`) {
-					url = `/`;
-				} else {
-					apiPath = `http://${req.headers.host}`;
-					url = `https://josephanimate2021.github.io/lvm-static/2014?api=${apiPath}&action=edit&movieId=${numId}`;
-				}
-				res.setHeader("Location", url);
-				res.end();
-			});
+					fs.unlinkSync(path);
+					res.statusCode = 302;
+					// why
+					var apiPath, url;
+					if (req.headers.host != "localhost" && req.headers.host != `localhost:${process.env.SERVER_PORT}`) {
+						url = `/`;
+					} else {
+						apiPath = `http://${req.headers.host}`;
+						url = `https://josephanimate2021.github.io/lvm-static/2014?api=${apiPath}&action=edit&movieId=${numId}`;
+					}
+					res.setHeader("Location", url);
+					res.end();
+				});
+			} catch (e) {
+				console.log("Error:", e);
+			}
 			return true;
 		}
 		case "/goapi/getInitParams/": {
-			loadPost(req, res).then(([data]) => console.log(data));
+			loadPost(req, res).then(([data]) => console.log(data)).catch(e => console.log("Error:", e));
 			return true;
 		}
 		default: return true;
