de0018ce405cfb29594f2dad41890e84f884f8ee
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-08-09 21:24:49 -0400

    Update load.js

--- watermark/load.js
+++ watermark/load.js
@@ -5,15 +5,19 @@ const http = require("http");
 
 module.exports = function (req, res, url) {
 	if (req.method != "POST" || url.path != "/goapi/getMovieInfo/") return; 
-	loadPost(req, res).then(([data]) => {
-		res.setHeader("Content-Type", "text/html; charset=UTF-8");
-		const p = `${folder}/${data.movieId}.xml`;
-		if (!fs.existsSync(p)) {
-			// don't load any watermarks if a user watermark does not exist.
-			res.end('<watermarks></watermarks>');
-		} else {
-			fs.createReadStream(p).pipe(res);
-		}
-	});
+	try {
+		loadPost(req, res).then(([data]) => {
+			res.setHeader("Content-Type", "text/html; charset=UTF-8");
+			const p = `${folder}/${data.movieId}.xml`;
+			if (!fs.existsSync(p)) {
+				// don't load any watermarks if a user watermark does not exist.
+				res.end('<watermarks><watermark style="josephanimate"/></watermarks>');
+			} else {
+				fs.createReadStream(p).pipe(res);
+			}
+		});
+	} catch (e) {
+		console.log("Error:", e);
+	}
 	return true;
 };
